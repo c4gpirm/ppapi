cae538bb352065637ef7b0926fe290f2747ec86c
cemdem <cdemirayak@Care4.be>
2023-03-24 14:33:13 +0100

    added predcit endpoint

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, request
+import io
 import json
+from flask import Flask, request, jsonify
+from PIL import Image
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_api_consumer():
     return "<p>Hello, Api consumer!</p>"
+
 
 @app.route('/getsampledata', methods=['GET'])
 def get_sample_data():
@@ -17,7 +21,7 @@ def get_sample_data():
         {"type": "WMV", "prob": 89}
     ]
 
-    bluriness = 2
+    bluriness = 3
 
     sampledata = [
         insects_and_probs,
@@ -25,3 +29,15 @@ def get_sample_data():
     ]
 
     return json.dumps(sampledata)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # ensure an image was properly uploaded to our endpoint
+    if request.files.get("image"):
+        # read the image in PIL format
+        image = request.files["image"].read()
+        image = Image.open(io.BytesIO(image))
+        return jsonify(image)
+    else:
+        return 'NOK'
